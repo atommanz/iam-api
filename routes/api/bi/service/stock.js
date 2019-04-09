f11a404b24c831c0a06259277944cb2df6adec01
atomercc <atomerns@gmail.com>
2019-04-09 15:42:38 +0700

    fin vew 0.1

--- routes/api/bi/service/stock.js
+++ routes/api/bi/service/stock.js
@@ -1,11 +1,12 @@
 import { sequelizePostgres, pool, sequelize } from '../../../../utils'
 import config from '../../../../config/config'
 import moment from 'moment'
-import fs from 'fs'
+import fs, { copyFileSync } from 'fs'
 import { POINT_CONVERSION_COMPRESSED, WSAESOCKTNOSUPPORT } from 'constants';
 // import { pool } from '../../../../utils'
 const branchList = config.branch
 const slocKeyList = config.slocKey
+const outputTemplate = config.outputStock
 
 const getListArticleIAM = async () => {
     const query = 'select * from sap_mara order by article_no ASC LIMIT 2'
@@ -142,53 +143,66 @@ const caseZBO2 = async (articleNo, date) => {
 
     const VarBeforeFindMinQty = await buildVarBeforeFindMinQty(ZBO1useMat, totalDivideQty)
     fs.writeFileSync("VarBeforeFindMinQty.json", JSON.stringify(VarBeforeFindMinQty))
-    // const finalStockListByBranch = []
-    // const promMap3 = branchList.map((valueBranch,index1)=>{
-    //     const tmpObj2 = {}
-    //     tmpObj2.plant = valueBranch
-    //     tmpObj2.stock = []
-    //     const promMap4 = totalDivideQty.map((value2,index2)=>{
-            
-    //     })
-    // })
 
+    const listMinStock = await findMin(VarBeforeFindMinQty)
+    fs.writeFileSync("listMinStock.json", JSON.stringify(listMinStock))
 
-    return articleNo
+    return listMinStock
 }
-const buildVarBeforeFindMinQty = async (ZBO1useMat,totalDivideQty) => {
-    
-    // ZBO1useMat[0].netQty = '3'
-    const stockDivideQtyUseMat = []
-    
-    const promMap = ZBO1useMat.map(async (value1, index1) => {
-        const objSubDivideQtyUseMat = {}
-        objSubDivideQtyUseMat.articleComponentNo = value1.component_article_no
-        objSubDivideQtyUseMat.stock = []
-        const resultSearchObject = searchByArticle(value1.component_article_no, componentStock)
-        // console.log(resultSearchObject)
-        const promMap2 = resultSearchObject.stock.map(async (value2, index2) => {
-            value2.sloc001 = Math.floor(Number(value2.sloc001) / Number(value1.netQty))
-            value2.sloc002 = Math.floor(Number(value2.sloc002) / Number(value1.netQty))
-            value2.sloc003 = Math.floor(Number(value2.sloc003) / Number(value1.netQty))
-            value2.sloc004 = Math.floor(Number(value2.sloc004) / Number(value1.netQty))
 
-            objSubDivideQtyUseMat.stock.push(value2)
-        })
-        await Promise.all(promMap2)
-        // console.log('round down',objSubDivideQtyUseMat)
-        stockDivideQtyUseMat.push(objSubDivideQtyUseMat)
+const findMin = async (inputList) => {
+    const outputList = []
+    const promMap = inputList.map((value, index) => {
+        // value.plant
+        const obj = {}
+        obj.plant = value.plant
+        obj.sloc001 = Math.min.apply(null, value.sloc001)
+        obj.sloc002 = Math.min.apply(null, value.sloc002)
+        obj.sloc003 = Math.min.apply(null, value.sloc003)
+        obj.sloc004 = Math.min.apply(null, value.sloc004)
+        // console.log(value.sloc001, Math.min.apply(null, value.sloc001))
+        // value.sloc001 = th.min.apply(null, value.sloc001)
+        outputList.push(obj)
     })
     await Promise.all(promMap)
-    
-    // console.log('stockDivideQtyUseMat',stockDivideQtyUseMat)
-    return stockDivideQtyUseMat
+    // console.log(outputList)
+    return outputList
+}
+
+const buildVarBeforeFindMinQty = async (ZBO1useMat, totalDivideQty) => {
+    const listOut = outputTemplate
+    const objOut = {}
+    const promMap = ZBO1useMat.map(async (value1, index1) => {
+
+        // objOut.plant = []
+
+        const resultSearchObject = searchByArticle(value1.component_article_no, totalDivideQty)
+
+
+        const promMap2 = branchList.map(async (value2, index2) => {
+
+
+            const resultSearchObjectByPlant = searchByPlant(value2, resultSearchObject.stock)
+
+            const objIndex = listOut.findIndex((obj => obj.plant == value2))
+            listOut[objIndex].sloc001.push(resultSearchObjectByPlant.sloc001)
+            listOut[objIndex].sloc002.push(resultSearchObjectByPlant.sloc002)
+            listOut[objIndex].sloc003.push(resultSearchObjectByPlant.sloc003)
+            listOut[objIndex].sloc004.push(resultSearchObjectByPlant.sloc004)
+        })
+        await Promise.all(promMap2)
+        // console.log(listOut)
+    })
+    await Promise.all(promMap)
+
+    return listOut
 }
 
 const findTotalStockByQty = async (ZBO1useMat, componentStock) => {
-    
+
     // ZBO1useMat[0].netQty = '3'
     const stockDivideQtyUseMat = []
-    
+
     const promMap = ZBO1useMat.map(async (value1, index1) => {
         const objSubDivideQtyUseMat = {}
         objSubDivideQtyUseMat.articleComponentNo = value1.component_article_no
@@ -209,7 +223,7 @@ const findTotalStockByQty = async (ZBO1useMat, componentStock) => {
         stockDivideQtyUseMat.push(objSubDivideQtyUseMat)
     })
     await Promise.all(promMap)
-    
+
     // console.log('stockDivideQtyUseMat',stockDivideQtyUseMat)
     return stockDivideQtyUseMat
 }
@@ -225,7 +239,7 @@ function searchByArticle(nameKey, myArray) {
 
 function searchByPlant(nameKey, myArray) {
     for (var i = 0; i < myArray.length; i++) {
-        if (myArray[i].articleComponentNo === nameKey) {
+        if (myArray[i].plant === nameKey) {
             return myArray[i];
         }
     }
@@ -250,23 +264,27 @@ const mainStock = async (aritcleNo, date) => {
     if (fs.existsSync('VarBeforeFindMinQty.json')) {
         fs.unlinkSync('VarBeforeFindMinQty.json')
     }
-    
-    
+
+
     const articleType = await getArticleType(aritcleNo)
     console.log('articleType', articleType)
     if (articleType === 'ZBO2') {
-        const aa = await caseZBO2(aritcleNo, date)
+        const output = await caseZBO2(aritcleNo, date)
+        return output
     }
     else if (articleType === 'ZBO1') {
-
+        const output = []
+        return output
     }
     else if (articleType === 'ZNM1') {
-
+        const output = []
+        return output
     }
     else {
-
+        const output = []
+        return output
     }
-    return articleType
+
 }
 
 export default {
